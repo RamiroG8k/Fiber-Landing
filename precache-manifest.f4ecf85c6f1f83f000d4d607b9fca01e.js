1d468dc05cf390afb306125c3e86dc2cfaa2a2c6
Ramiro Mendez <ramiromendez@XIFRA-SIS006s-MacBook-Pro.local>
2021-09-20 10:36:28 -0500

    Updates

---
+++ precache-manifest.f4ecf85c6f1f83f000d4d607b9fca01e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1640084d5161841330d81242ca347cce",
+    "url": "/fiber-landing/index.html"
+  },
+  {
+    "revision": "a9f9a39077bf589f4879",
+    "url": "/fiber-landing/static/css/main.bfeb3117.chunk.css"
+  },
+  {
+    "revision": "883643cad2309bd4e1ef",
+    "url": "/fiber-landing/static/js/2.857cff89.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/fiber-landing/static/js/2.857cff89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9f9a39077bf589f4879",
+    "url": "/fiber-landing/static/js/main.54b81956.chunk.js"
+  },
+  {
+    "revision": "4eb87fb16834b82734af",
+    "url": "/fiber-landing/static/js/runtime-main.fd12d4e1.js"
+  }
+]);
